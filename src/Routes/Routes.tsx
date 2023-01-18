72eef471d1c0a8425b061be7938323778dd71ee7
devsrabon <et.srabon@gmail.com>
2023-01-18 14:11:00 +0600

    added payment mathod

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -2,19 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 
 import Home from "../pages/Home/Home";
+import Booking from "../pages/Home/Booking/Booking";
 
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <Main/>,
+		element: <Main />,
 		children: [
 			{
 				path: "/",
-				element: <Home/>,
+				element: <Home />,
 			},
-			
-			
+			{
+				path: "/book/:id",
+				element: <Booking />,
+				loader:({params})=> fetch(`${process.env.REACT_APP_API_URL}/book/${params.id}`),
+			},
 		],
 	},
 ]);

e9b49bb6e41326352b15831e318191743e0d204f
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-01-20 17:54:22 +0300

    question answer

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
-
 import Home from "../pages/Home/Home";
 import Booking from "../pages/Home/Booking/Booking";
 import Login from "../pages/login/Login";
@@ -9,67 +8,71 @@ import PrivateRoute from "./PrivateRoute";
 import TopQuestion from "../pages/Home/TopQuestions/TopQuestion";
 import QuestionAns from "../pages/QuestionAns/QuestionAns";
 import NotFound from "../pages/404";
-import SingleQnA from "../pages/QuestionAns/SingleQ
+import SingleQnA from "../pages/QuestionAns/SingleQnA";
 
 export const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Main />,
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "/book/:id",
-				element: (
-					<PrivateRoute>
-						<Booking />
-					</PrivateRoute>
-				),
-				loader: ({ params }) =>
-					fetch(`${process.env.REACT_APP_API_URL}/book/${params.id}`),
-			},
-			{
-				path: "/topquestions/:id",
-				element: (
-					<PrivateRoute>
-						<TopQuestion />
-					</PrivateRoute>
-				),
-				loader: ({ params }) =>
-					fetch(`${process.env.REACT_APP_API_URL}/topquestions/${params.id}`),
-			},
-			{
-				path: "qna",
-				loader: () => fetch("https://skill-judge-server.vercel.app/qna"),
-				element: <QuestionAns></QuestionAns>,
-			},
-			{
-				path: "/login",
-				element: <Login />
-			},
-			{
-				path: '/signup',
-
-				element: <Signup />
-			}
-			element: <Signup/>},
+    {
+        path: "/",
+        element: <Main />,
+        children: [
             {
-        path: "/qna",
-        loader: () => fetch(`${process.env.REACT_APP_API_URL}/qna`),
-        element: <QuestionAns></QuestionAns>,
-      },
-      {
-        path: "/singleqna/:id",
-        loader: ({ params }) =>
-          fetch(`${process.env.REACT_APP_API_URL}/qnasingle/${params.id}`),
-        element:<SingleQnA></SingleQnA>,
-      },
-		],
-	},
-	{
-		path: '*',
-		element: <NotFound></NotFound>
-	}
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/book/:id",
+                element: (
+                    <PrivateRoute>
+                        <Booking />
+                    </PrivateRoute>
+                ),
+                loader: ({ params }) =>
+                    fetch(`${process.env.REACT_APP_API_URL}/book/${params.id}`),
+            },
+            {
+                path: "/topquestions/:id",
+                element: (
+                    <PrivateRoute>
+                        <TopQuestion />
+                    </PrivateRoute>
+                ),
+                loader: ({ params }) =>
+                    fetch(
+                        `${process.env.REACT_APP_API_URL}/topquestions/${params.id}`
+                    ),
+            },
+            {
+                path: "qna",
+                loader: () =>
+                    fetch("https://skill-judge-server.vercel.app/qna"),
+                element: <QuestionAns></QuestionAns>,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+
+                element: <Signup />,
+            },
+            {
+                path: "/qna",
+                loader: () => fetch(`${process.env.REACT_APP_API_URL}/qna`),
+                element: <QuestionAns></QuestionAns>,
+            },
+            {
+                path: "/singleqna/:id",
+                loader: ({ params }) =>
+                    fetch(
+                        `${process.env.REACT_APP_API_URL}/qnasingle/${params.id}`
+                    ),
+                element: <SingleQnA></SingleQnA>,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <NotFound></NotFound>,
+    },
 ]);
